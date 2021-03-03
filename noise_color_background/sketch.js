5962edf5be888d48b6a27f908b5ccf4ec749b2e6
Absax <abhishekgautam011@gmail.com>
2021-03-04 00:20:32 +0530

    don't really know what to make of this

--- noise_color_background/sketch.js
+++ noise_color_background/sketch.js
@@ -11,23 +11,23 @@ function draw() {
 
     let alpha = 200;
     let t = frameCount / 200;
-    background(255 * noise(t), 255 * noise(t*t), 255 * noise(sqrt(t)), 200);
+    background(255);
     
 
     for (let x = 0; x < width; x += 10) {
         for (let y = 0; y < height; y += 10) {
             let nse;
-            if (x < width / 2) {
+            if (y < height / 2) {
                 nse = noise(x / alpha, y / alpha, t);
             } else {
-                nse = noise((width - x) / alpha, y / alpha, t);
+                nse = noise(x / alpha, (height-y) / alpha, t);
             }
-            if (nse < 0.5) {
+            if (nse < 0.6) {
                 // fill(255 * noise(x / alpha), 255 * noise(y / alpha), 255 * noise(t), 200);
-                // circle(x, y, 40);
+                // circle(x, y, 20);
             }
             else{
-                fill(255 * (1-noise(x / alpha)), 255 * (1-noise(y / alpha)), 255 * (1-noise(t)), 200);
+                fill(255 * (1-noise(x / alpha)), 255 * (1-noise(y / alpha)), 255 * (1-noise(t)), 150);
                 circle(x, y, 20);
             }
         }
