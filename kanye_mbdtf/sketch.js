acab6e6d1db0d88b1052dbc6b8faaeedb14480a3
Absax <abhishekgautam011@gmail.com>
2021-03-24 01:11:24 +0530

    'My beautiful Dark Twisted Fantasty'

--- kanye_mbdtf/sketch.js
+++ kanye_mbdtf/sketch.js
@@ -1,69 +1,55 @@
-var mass = [];
-var positionX = [];
-var velocityX = [];
-var positionY = [];
-var velocityY = [];
-var size = [];
-let img;
-let hue = [100, 25, 50, 75];
-let color = [];
-let lifetime = [];
+let imgs = [];
+let parental;
+let img_num = 5;
+let graphics;
+let w, h;
+let scl = 10;
+size = 250;
+border = 8;
 
 
+function preload() {
+    w = int(600);
+    h = int(600);
+
+    for (let i = 0; i < img_num; i++) {
+        let img = loadImage("https://loremflickr.com/" + w + "/" + h + "/kanye/?random=" + i,
+            function () { imgs.push(img); });
+    }
+    parental = loadImage("parental2.png");
+}
 
 function setup() {
-    colorMode(HSB, 100);
-    img = loadImage('among-us.png');
-    backgroundImage = loadImage('background.jpg');
-    createCanvas(window.innerWidth, window.innerHeight);
-    for (var particle = 0; particle < 100; particle++) {
-        velocityX[particle] = random(-1, 1);
-        velocityY[particle] = random(-1, 1);
-    }
+    createCanvas(w, h);
+    angleMode(DEGREES);
+    noStroke();
+    graphics = createGraphics(width, height);
+    graphics.colorMode(HSB, 360, 100, 100, 100);
+    graphics.angleMode(DEGREES)
 }
 
 function draw() {
-    background(0);
-    image(backgroundImage, 0, 0);
-    for (var particle = 0; particle < mass.length; particle++) {   
-        tint(color[particle], 100, 100, lifetime[particle]);
-        if(lifetime[particle] > 50 ) lifetime[particle] -= 0.5;
-        if(lifetime[particle] < 50 ) lifetime[particle] -= 10;
-        if(lifetime[particle] < 0) lifetime[particle] = 0;
-        image(img, positionX[particle], positionY[particle], size[particle], size[particle]);
-        noTint();
-        positionX[particle] += velocityX[particle];
-        positionY[particle] += velocityY[particle];
-    }
-    for( var particle = 0; particle < mass.length; particle++) {
-        if(positionX[particle] > width || positionX[particle] < 0 || positionY[particle] > height || positionY[particle] < 0){
-            mass.splice(particle, 1);
-            positionX.splice(particle, 1);
-            positionY.splice(particle, 1);
-            velocityX.splice(particle, 1);
-            velocityY.splice(particle, 1);
-            size.splice(particle, 1);
-            color.splice(particle, 1);
-        }
+    background('#EC2440');
+
+    graphics.clear();
+
+    for(let i = 0; i < img_num; i++)
+    {
+        fill('#faca0a');
+        rectMode(CENTER);
+        rect(300, 300, size + border, size + border);
+        graphics.image(random(imgs), 300, 300, 250, 250);
     }
 
-}
 
-function addParticles() {
-    color.push(random(hue));
-    mass.push(img);
-    size.push(random(20, 50));
-    positionX.push(mouseX);
-    positionY.push(mouseY);
-    velocityX.push(0);
-    velocityY.push(0);
-    lifetime.push(random(100, 200));
-}
+    // graphics.filter(POSTERIZE, 9);
+    // graphics.filter(THRESHOLD, ratio, 1-ratio);
+    // graphics.filter(INVERT);
+    push();
+    imageMode(CENTER);
+    image(graphics, 175, 175);
+    image(parental, 540, 560, 100, 100);
+    pop();
 
-function mouseClicked() {
-    addParticles();
-}
-
-function mouseDragged() {
-    addParticles();
+    frameRate(.5);
 }
